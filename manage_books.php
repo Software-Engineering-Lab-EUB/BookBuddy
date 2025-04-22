141a6262db5a4de0856d1e6c7e095464ca1a7c13
Noor Alam Islam Manik <nooralamislammanik@gmail.com>
2025-04-23 00:01:02 +0600

    Update manage_books.php

--- manage_books.php
+++ manage_books.php
@@ -215,5 +215,20 @@ $result = $conn->query("SELECT * FROM books");
         </div>
     </div>
 </div>
+
+<!-- Include jQuery and Bootstrap JS (if not already included) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('.thumbnail').on('click', function() {
+            var imageSrc = $(this).data('image');
+            $('#modalImage').attr('src', imageSrc);
+        });
+    });
+</script>
+
+<?php include "footer.php"; ?>
                 
 

7245e724af009f4ac59da1101355ae656192de0c
Noor Alam Islam Manik <nooralamislammanik@gmail.com>
2025-04-22 22:19:27 +0600

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -6,3 +6,14 @@ if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "admin") {
 }
 include "header.php";
 ?>
+
+<div class="container mt-5">
+    <h2 class="text-center">Admin Panel</h2>
+    <div class="list-group">
+        <a href="manage_books.php" class="list-group-item list-group-item-action">Book Management</a>
+        <a href="manage_orders.php" class="list-group-item list-group-item-action">Order Management</a>
+        <a href="manage_users.php" class="list-group-item list-group-item-action">User  Management</a>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>

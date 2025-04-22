33af90c06df5c99bdeeb793987abe968e85c3412
Noor Alam Islam Manik <nooralamislammanik@gmail.com>
2025-04-22 23:37:41 +0600

    Update manage_books.php

--- manage_books.php
+++ manage_books.php
@@ -7,6 +7,13 @@ if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "admin") {
 include "header.php";
 include "db.php";
 
+ // Insert the book into the database
+    $stmt = $conn->prepare("INSERT INTO books (title, author, price, stock, category_id, image, description) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssdisss", $title, $author, $price, $stock, $category_id, $imagePath, $description);
+    $stmt->execute();
+    header("Location: manage_books.php");
+    exit();
+
 <!-- Book List -->
     <table class="table table-striped">
         <thead>

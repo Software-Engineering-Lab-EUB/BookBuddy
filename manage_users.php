aa8eb21c3d51c074ea70fca5339056f4310adb21
Noor Alam Islam Manik <nooralamislammanik@gmail.com>
2025-04-23 21:04:33 +0600

    Update manage_users.php

--- manage_users.php
+++ manage_users.php
@@ -6,3 +6,16 @@ if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "admin") {
 }
 include "header.php";
 include "db.php";
+
+
+ <!-- User List -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Actions</th>
+            </tr>
+        </thead>

347a64bbe7faa51cfde111509c7ae76cc7a0020c
Mst. Alif Jan Nur Shiren <167988694+AJshiren@users.noreply.github.com>
2025-04-27 02:03:33 +0600

    Display message if cart is empty
    Display message if cart is empty

--- cart_view.php
+++ cart_view.php
@@ -4,4 +4,11 @@ include "db.php";
 include "header.php";
 
 <h2 class="text-center">Shopping Cart</h2>
+
+<?php
+if (!isset($_SESSION['cart']) || count($_SESSION['cart']) == 0) {
+    echo "<p>Your cart is empty.</p>";
+} else {
+  }
+?>
 ?>

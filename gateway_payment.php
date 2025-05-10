a01c6746d571de6bb8f96dbb661d0cce020037b1
Tanni <tannijannat61@gmail.com>
2025-05-10 17:18:54 +0600

    Update gateway_payment.php
    Add session check and header/footer inclusion for payment page

--- gateway_payment.php
+++ gateway_payment.php
@@ -1,3 +1,11 @@
-<?php include "footer.php"; ?>
-</body>
-</html>
+<?php
+session_start();
+include "header.php";
+
+// Check if checkout data is set
+if (!isset($_SESSION['checkout_data'])) {
+    echo "<div class='container mt-5 alert alert-danger text-center'>No payment session found.</div>";
+    include "footer.php";
+    exit();
+}
+?>

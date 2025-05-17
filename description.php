fc9841caab3549de86fdc20a268282e05240c32d
Noor Alam Islam Manik <nooralamislammanik@gmail.com>
2025-05-18 00:02:48 +0600

    Prepare and execute the query to fetch book details

--- description.php
+++ description.php
@@ -7,3 +7,9 @@ include "header.php";
 if (isset($_GET['id'])) {
     $book_id = $_GET['id'];
 
+// Prepare and execute the query to fetch book details
+    $stmt = $conn->prepare("SELECT * FROM books WHERE id = ?");
+    $stmt->bind_param("i", $book_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+

8acbc3831f7280d167e25fd204b4b3b32bcd673d
Mst. Alif Jan Nur Shiren <167988694+AJshiren@users.noreply.github.com>
2025-05-19 00:48:42 +0600

    initialize cart in session if not already set
    initialize cart in session if not already set

--- cart.php
+++ cart.php
@@ -7,7 +7,10 @@ include "db.php";
 // check if book ID is passed via GET request
 if (isset($_GET['id'])) {
     $book_id = $_GET['id'];
-
+// initialize cart in session if not already set
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = [];
+    }
 
 
 ?>

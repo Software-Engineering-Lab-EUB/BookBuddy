9a941047722b9103aceecf13fabe3efdcc73d08f
Mst. Alif Jan Nur Shiren <167988694+AJshiren@users.noreply.github.com>
2025-05-19 01:58:53 +0600

    nclude Bootstrap CSS and JS along with FontAwesome and jQuery

--- description.php
+++ description.php
@@ -156,6 +156,14 @@ function redirectToLogin() {
 }
 </script>
 
+<!--nclude Bootstrap CSS and JS along with FontAwesome and jQuery -->
+<!-- Bootstrap & JS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+
          
 
 

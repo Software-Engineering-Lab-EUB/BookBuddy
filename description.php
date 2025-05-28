86f33db8905a9f50842c5ca6970de1b794de906e
Mst. Alif Jan Nur Shiren <167988694+AJshiren@users.noreply.github.com>
2025-05-28 22:10:05 +0600

    Get book ID from URL

--- description.php
+++ description.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
 
+// Include database config
+include "db.php";
+
+// Get book ID from URL
+$book_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+if ($book_id <= 0) {
+    echo "Invalid book ID.";
+    exit;
+}
